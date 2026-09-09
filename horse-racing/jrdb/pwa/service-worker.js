98d1162f6a7b8b956bb4d1ee68dd786db11ad54d
yukki0113 <iharacchiy@gmail.com>
2026-09-09 18:30:30 +0900

    chore(jrdb): deploy Newspaper day restore v2

--- horse-racing/jrdb/pwa/service-worker.js
+++ horse-racing/jrdb/pwa/service-worker.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const CACHE_NAME = "jrdb-pwa-shell-v27";
+const CACHE_NAME = "jrdb-pwa-shell-v28";
 const APP_SHELL = [
   "./",
   "./index.html",
@@ -14,7 +14,7 @@ const APP_SHELL = [
   "./fact-lite-sort.js?v=18",
   "./newspaper.js?v=2",
   "./newspaper-v2.js?v=3",
-  "./newspaper-day.js?v=1",
+  "./newspaper-day.js?v=2",
   "./newspaper-v4.js?v=7",
   "./manifest.webmanifest",
   "./vendor/sql-wasm.js",
