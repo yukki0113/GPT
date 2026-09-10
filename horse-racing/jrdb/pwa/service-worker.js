c23be39843b237367081edd65f322d0dcc2615f3
yukki0113 <iharacchiy@gmail.com>
2026-09-11 00:58:22 +0900

    chore(jrdb): refresh PWA cache for special memo detail UI

--- horse-racing/jrdb/pwa/service-worker.js
+++ horse-racing/jrdb/pwa/service-worker.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const CACHE_NAME = "jrdb-pwa-shell-v36";
+const CACHE_NAME = "jrdb-pwa-shell-v37";
 const APP_SHELL = [
   "./",
   "./index.html",
@@ -19,7 +19,7 @@ const APP_SHELL = [
   "./newspaper-v4.js?v=7",
   "./newspaper-v5.js?v=1",
   "./newspaper-v6.js?v=1",
-  "./newspaper-v7.js?v=2",
+  "./newspaper-v7.js?v=3",
   "./manifest.webmanifest",
   "./vendor/sql-wasm.js",
   "./vendor/sql-wasm.wasm"
