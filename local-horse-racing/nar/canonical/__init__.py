dfa8581f4c2a1ca7ae031f84ed8828195082fb27
yukki0113 <iharacchiy@gmail.com>
2026-09-17 15:07:51 +0900

    noop

--- local-horse-racing/nar/canonical/__init__.py
+++ local-horse-racing/nar/canonical/__init__.py
@@ -3,6 +3,15 @@
 from .field_catalog import FIELD_CATALOG, catalog_rows
 from .leakage import validate_history_asof
 from .parser import CanonicalBatch, parse_monthly_race_zip
+from .reconciliation import parse_monthly_race_zip_reconciled
 from .schema import TABLE_SPECS
 
-__all__ = ["CanonicalBatch", "FIELD_CATALOG", "TABLE_SPECS", "catalog_rows", "parse_monthly_race_zip", "validate_history_asof"]
+__all__ = [
+    "CanonicalBatch",
+    "FIELD_CATALOG",
+    "TABLE_SPECS",
+    "catalog_rows",
+    "parse_monthly_race_zip",
+    "parse_monthly_race_zip_reconciled",
+    "validate_history_asof",
+]
